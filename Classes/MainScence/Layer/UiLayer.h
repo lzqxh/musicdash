2af6599435f3a44feb0fd71c23b52a09fe956538
Alan Guo <guoang.sysu@gmail.com>
2014-12-07 21:30:04 +0800

    touch effect

--- Classes/MainScence/Layer/UiLayer.h
+++ Classes/MainScence/Layer/UiLayer.h
@@ -2,6 +2,7 @@
 #define __UI_LAYER_H__
 
 #include "cocos2d.h"
+#include <vector>
 
 USING_NS_CC;
 
@@ -9,5 +10,14 @@ class UiLayer : public Layer {
 public:
 	virtual bool init();
 	CREATE_FUNC(UiLayer);
+
+	virtual bool onTouchBegan(Touch *touch, Event *event) override;
+	virtual void onTouchMoved(Touch *touch, Event *event) override;
+	virtual void onTouchEnded(Touch *touch, Event *event) override;
+	virtual void update(float dt) override;
+
+private:
+	std::vector<Sprite *>ts;
+	MotionStreak * ms;
 };
 #endif //__UI_LAYER_H__
